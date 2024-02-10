c70a1b2b58bb52f65acfa8ff5ee5128aef8016cd
Speedy <91589360+Darklightning2008@users.noreply.github.com>
2024-02-10 08:14:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 
-BOT_TOKEN = 'YOUR_BOT_TOKEN'
-MONGO_URI = 'mongodb://username:password@localhost:27017/'
-TELEGRAM_API_ID = 'YOUR_TELEGRAM_API_ID'
-TELEGRAM_API_HASH = 'YOUR_TELEGRAM_API_HASH'
+BOT_TOKEN = '6402280578:AAEeDZ8H8OMB-Yh6TiM21HG_pSd89JJA3qo'
+MONGO_URI = 'mongodb+srv://DarkLightning2008:Darkdeebu2008@cluster1.ut0l8mm.mongodb.net/?retryWrites=true&w=majority/'
+TELEGRAM_API_ID = 22839370
+TELEGRAM_API_HASH = '7fb99db07ddecec3e7a1f02da9730c33'
