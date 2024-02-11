90ed0bca18c65f109395d1a00ad587e8aca1c560
Speedy <91589360+Darklightning2008@users.noreply.github.com>
2024-02-11 08:37:22 +0530

    Update naruto_bot.py

--- bot/naruto_bot.py
+++ bot/naruto_bot.py
@@ -21,11 +21,18 @@ client = Client(
 db_client = MongoClient(MONGO_URI)
 db = db_client['naruto_game']
 
-# Sudo users who can use /add, /edit, /clear
-sudo_users = set()
-
 # Owners who can add sudo users
 owners = {6916220465}  # Add the specified user ID to the owners set
+
+def load_sudo_users():
+    sudo_users_data = db.settings.find_one({'key': 'sudo_users'})
+    return set(sudo_users_data['value']) if sudo_users_data else set()
+
+def save_sudo_users(sudo_users_set):
+    db.settings.update_one({'key': 'sudo_users'}, {'$set': {'value': list(sudo_users_set)}}, upsert=True)
+
+# Load sudo users on startup
+sudo_users = load_sudo_users()
 
 def get_role_link(role):
     if role == "leader":
@@ -42,11 +49,10 @@ def start_handler(client, message):
     start_text = (
         "Hi! I am a bot to maintain deposits in Uzumaki clan.\n\n"
         "Use the following commands:\n"
-        "/add {name} {currency} {amount} {transaction_type}\n"
-        "/edit {name} {deposit/loan} {currency} {amount}\n"
+        "/add_deposit {name} {gems/tokens/coins} {amount}\n"
+        "/add_loan {name} {gems/tokens/coins} {amount}\n"
+        "/edit {name} {deposit/loan} {gems/tokens/coins} {amount}\n"
         "/list\n"
-        "/deposit\n"
-        "/loan"
     )
 
     keyboard = InlineKeyboardMarkup(
@@ -70,6 +76,7 @@ def addsudo_handler(client, message):
     if is_owner(message.from_user.id):
         user_id = message.text.split()[1]
         sudo_users.add(int(user_id))
+        save_sudo_users(sudo_users)
         message.reply_text(f'Successfully added user with ID {user_id} to sudo users.')
     else:
         message.reply_text('You are not authorized to use this command.')
@@ -83,16 +90,29 @@ def addowner_handler(client, message):
     else:
         message.reply_text('You are not authorized to use this command.')
 
-@client.on_message(filters.command("add") & (filters.private | filters.group))
-def add_handler(client, message):
+@client.on_message(filters.command("add_deposit") & (filters.private | filters.group))
+def add_deposit_handler(client, message):
     if is_sudo_user(message.from_user.id):
         args = message.text.split()[1:]
-        if len(args) == 4:
-            name, currency, amount, transaction_type = args
-            db.deposits.insert_one({'name': name, 'currency': currency, 'amount': amount, 'type': transaction_type})
-            message.reply_text(f'Successfully added {amount} {currency} {transaction_type} for {name}')
+        if len(args) == 3:
+            name, currency, amount = args
+            db.deposits.insert_one({'name': name, 'currency': currency, 'amount': amount, 'type': 'deposit'})
+            message.reply_text(f'Successfully added {amount} {currency} to deposit for {name}')
         else:
-            message.reply_text('Invalid command format. Use /add {name} {currency} {amount} {transaction_type}')
+            message.reply_text('Invalid command format. Use /add_deposit {name} {gems/tokens/coins} {amount}')
+    else:
+        message.reply_text('You are not authorized to use this command.')
+
+@client.on_message(filters.command("add_loan") & (filters.private | filters.group))
+def add_loan_handler(client, message):
+    if is_sudo_user(message.from_user.id):
+        args = message.text.split()[1:]
+        if len(args) == 3:
+            name, currency, amount = args
+            db.deposits.insert_one({'name': name, 'currency': currency, 'amount': amount, 'type': 'loan'})
+            message.reply_text(f'Successfully added {amount} {currency} to loan for {name}')
+        else:
+            message.reply_text('Invalid command format. Use /add_loan {name} {gems/tokens/coins} {amount}')
     else:
         message.reply_text('You are not authorized to use this command.')
 
@@ -109,14 +129,6 @@ def edit_handler(client, message):
     else:
         message.reply_text('You are not authorized to use this command.')
 
-@client.on_message(filters.command("clear") & (filters.private | filters.group))
-def clear_handler(client, message):
-    if is_sudo_user(message.from_user.id):
-        db.deposits.delete_many({})
-        message.reply_text('All records have been cleared.')
-    else:
-        message.reply_text('You are not authorized to use this command.')
-
 @client.on_message(filters.command("list") & (filters.private | filters.group))
 def list_handler(client, message):
     deposit_list = db.deposits.find({'type': 'deposit'})
@@ -129,6 +141,20 @@ def list_handler(client, message):
     loan_text = '\n'.join(loan_formatted_list) if loan_formatted_list else 'No loan records found.'
 
     message.reply_text(f"Deposit records:\n{deposit_text}\n\nLoan records:\n{loan_text}")
+
+@client.on_message(filters.command("deposit") & (filters.private | filters.group))
+def deposit_handler(client, message):
+    deposit_list = db.deposits.find({'type': 'deposit'})
+    deposit_formatted_list = [f"{item['name']} {item['currency']} {item['amount']}" for item in deposit_list]
+    deposit_text = '\n'.join(deposit_formatted_list) if deposit_formatted_list else 'No deposit records found.'
+    message.reply_text(f"Deposit records:\n{deposit_text}")
+
+@client.on_message(filters.command("loan") & (filters.private | filters.group))
+def loan_handler(client, message):
+    loan_list = db.deposits.find({'type': 'loan'})
+    loan_formatted_list = [f"{item['name']} {item['currency']} {item['amount']}" for item in loan_list]
+    loan_text = '\n'.join(loan_formatted_list) if loan_formatted_list else 'No loan records found.'
+    message.reply_text(f"Loan records:\n{loan_text}")
 
 def is_owner(user_id):
     return user_id in owners
