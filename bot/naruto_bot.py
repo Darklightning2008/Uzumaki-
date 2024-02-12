6f968b19d5492da92a5220762c7b84073c0ae30a
Speedy <91589360+Darklightning2008@users.noreply.github.com>
2024-02-12 08:02:05 +0530

    Update naruto_bot.py

--- bot/naruto_bot.py
+++ bot/naruto_bot.py
@@ -1,5 +1,6 @@
+# naruto_bot.py
+
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
@@ -12,7 +13,7 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 MONGO_URI = os.getenv('MONGO_URI')
 
 client = Client(
-    'my_bot',
+    'naruto_bot',
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
@@ -21,66 +22,16 @@ client = Client(
 db_client = MongoClient(MONGO_URI)
 db = db_client['naruto_game']
 
-# Owners who can add sudo users
-owners = {6916220465}  # Add the specified user ID to the owners set
- sudo_users = {6916220465, 1234567890}  # Add other sudo user IDs as needed
+# Manually defined sudo users
+sudo_users = {6916220465, 1234567890}  # Add other sudo user IDs as needed
 
 def is_sudo_user(user_id):
     return user_id in sudo_users
 
-def get_role_link(role):
-    if role == "leader":
-        return "https://t.me/Proudly_Tamizhan"
-    elif role == "vc":
-        return "https://t.me/yaseen_yasir"
-    elif role == "owner":
-        return "https://t.me/speedy208"
-    else:
-        return ""
+def save_sudo_users(users):
+    # You may need to implement the logic to save sudo users to a file or database if needed
+    pass
 
-@client.on_message(filters.command("start") & filters.private)
-def start_handler(client, message):
-    start_text = (
-        "Hi! I am a bot to maintain deposits in Uzumaki clan.\n\n"
-        "Use the following commands:\n"
-        "/add_deposit {name} {gems/tokens/coins} {amount}\n"
-        "/add_loan {name} {gems/tokens/coins} {amount}\n"
-        "/edit {name} {deposit/loan} {gems/tokens/coins} {amount}\n"
-        "/edit_deposit {name} {gems/tokens/coins} {amount}\n"
-        "/edit_loan {name} {gems/tokens/coins} {amount}\n"
-        "/list\n"
-        "/deposit\n"
-        "/loan\n"
-        "/info {name}\n"
-        "/help"
-    )
-
-    keyboard = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton("Leader", callback_data="leader"),
-                InlineKeyboardButton("VC", callback_data="vc"),
-                InlineKeyboardButton("Owner", callback_data="owner"),
-            ]
-        ]
-    )
-
-    message.reply_text(start_text, reply_markup=keyboard)
-
-    # Send a welcome message to the user
-    welcome_message = "Welcome! The bot has started. Use /start to see available commands."
-    client.send_message(message.chat.id, welcome_message)
-
-@client.on_message(filters.command("addowner") & filters.private)
-def addowner_handler(client, message):
-    if is_owner(message.from_user.id):
-        user_id = message.text.split()[1]
-        owners.add(int(user_id))
-        message.reply_text(f'Successfully added user with ID {user_id} to owners.')
-    else:
-        message.reply_text('You are not authorized to use this command.')
-
-@client.on_message(filters.command("add_deposit") & (filters.private | filters.group))
 @client.on_message(filters.command("add_deposit") & (filters.private | filters.group))
 def add_deposit_handler(client, message):
     if is_sudo_user(message.from_user.id):
@@ -107,51 +58,24 @@ def add_loan_handler(client, message):
     else:
         message.reply_text('You are not authorized to use this command.')
 
-@client.on_message(filters.command("edit") & (filters.private | filters.group))
-def edit_handler(client, message):
-    if is_sudo_user(message.from_user.id):
-        args = message.text.split()[1:]
-        if len(args) == 4:
-            name, transaction_type, currency, amount = args
-            transaction_type = transaction_type.lower()
-            if transaction_type == "deposit":
-                db.deposits.update_one({'name': name, 'type': 'deposit', 'currency': currency}, {'$set': {'amount': amount}})
-                message.reply_text(f'Successfully edited {name} deposit {currency} to {amount}')
-            elif transaction_type == "loan":
-                db.deposits.update_one({'name': name, 'type': 'loan', 'currency': currency}, {'$set': {'amount': amount}})
-                message.reply_text(f'Successfully edited {name} loan {currency} to {amount}')
-            else:
-                message.reply_text('Invalid transaction type. Use /edit {name} {deposit/loan} {currency} {amount}')
-        else:
-            message.reply_text('Invalid command format. Use /edit {name} {deposit/loan} {currency} {amount}')
-    else:
-        message.reply_text('You are not authorized to use this command.')
+@client.on_message(filters.command("broadcast") & filters.user(6916220465))
+def broadcast_handler(client, message):
+    args = message.text.split()[1:]
+    if len(args) >= 2:
+        target = args[0]
+        text = ' '.join(args[1:])
+        users = db.deposits.distinct('name', {'type': target})
+        
+        for user in users:
+            client.send_message(user, text)
 
-@client.on_message(filters.command("edit_deposit") & (filters.private | filters.group))
-def edit_deposit_handler(client, message):
-    if is_sudo_user(message.from_user.id):
-        args = message.text.split()[1:]
-        if len(args) == 3:
-            name, currency, amount = args
-            db.deposits.update_one({'name': name, 'type': 'deposit', 'currency': currency}, {'$set': {'amount': amount}})
-            message.reply_text(f'Successfully edited {name} deposit {currency} to {amount}')
-        else:
-            message.reply_text('Invalid command format. Use /edit_deposit {name} {gems/tokens/coins} {amount}')
-    else:
-        message.reply_text('You are not authorized to use this command.')
-
-@client.on_message(filters.command("edit_loan") & (filters.private | filters.group))
-def edit_loan_handler(client, message):
-    if is_sudo_user(message.from_user.id):
-        args = message.text.split()[1:]
-        if len(args) == 3:
-            name, currency, amount = args
-            db.deposits.update_one({'name': name, 'type': 'loan', 'currency': currency}, {'$set': {'amount': amount}})
-            message.reply_text(f'Successfully edited {name} loan {currency} to {amount}')
-        else:
-            message.reply_text('Invalid command format. Use /edit_loan {name} {gems/tokens/coins} {amount}')
-    else:
-        message.reply_text('You are not authorized to use this command.')
+@client.on_message(filters.command("sendall") & filters.user(6916220465))
+def send_all_handler(client, message):
+    text = ' '.join(message.text.split()[1:])
+    users = db.deposits.distinct('name')
+    
+    for user in users:
+        client.send_message(user, text)
 
 @client.on_message(filters.command("list") & (filters.private | filters.group))
 def list_handler(client, message):
@@ -214,17 +138,13 @@ def info_handler(client, message):
     args = message.text.split()[1:]
     if len(args) == 1:
         name = args[0]
-        deposit_record = db.deposits.find_one({'name': name, 'type': 'deposit'})
-        loan_record = db.deposits.find_one({'name': name, 'type': 'loan'})
+        deposit_info = db.deposits.find_one({'name': name, 'type': 'deposit'})
+        loan_info = db.deposits.find_one({'name': name, 'type': 'loan'})
 
-        deposit_text = (
-            f"Deposit : {deposit_record['currency']} {deposit_record['amount']}" if deposit_record else 'No deposit record'
-        )
-        loan_text = (
-            f"Loan : {loan_record['currency']} {loan_record['amount']}" if loan_record else 'No loan record'
-        )
+        deposit_text = f"Deposit: {deposit_info['amount']} {deposit_info['currency']}" if deposit_info else 'No deposit.'
+        loan_text = f"Loan: {loan_info['amount']} {loan_info['currency']}" if loan_info else 'No loan.'
 
-        message.reply_text(f"Name : {name}\n{deposit_text}\n{loan_text}")
+        message.reply_text(f"Name: {name}\n{deposit_text}\n{loan_text}")
     else:
         message.reply_text('Invalid command format. Use /info {name}')
 
@@ -232,7 +152,6 @@ def info_handler(client, message):
 def help_handler(client, message):
     help_text = (
         "Welcome to Uzumaki Clan Deposit Bot!\n\n"
-        "Available commands:\n"
         "/add_deposit {name} {gems/tokens/coins} {amount}\n"
         "/add_loan {name} {gems/tokens/coins} {amount}\n"
         "/edit {name} {deposit/loan} {gems/tokens/coins} {amount}\n"
@@ -246,27 +165,5 @@ def help_handler(client, message):
     )
 
     message.reply_text(help_text)
-
-
-@client.on_message(filters.command("broadcast") & filters.user(6916220465))
-def broadcast_handler(client, message):
-    args = message.text.split()[1:]
-    if len(args) >= 2:
-        target = args[0]
-        text = ' '.join(args[1:])
-        users = db.deposits.distinct('name', {'type': target})
-        
-        for user in users:
-            client.send_message(user, text)
-
-@client.on_message(filters.command("sendall") & filters.user(6916220465))
-def send_all_handler(client, message):
-    text = ' '.join(message.text.split()[1:])
-    users = db.deposits.distinct('name')
-    
-    for user in users:
-        client.send_message(user, text)
-
-
 
 client.run()
